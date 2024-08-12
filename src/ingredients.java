822d6bde6ccbf00c91bb1ede4dfbb81052ab0e99
Виктория <sveckavika@mail.ru>
2024-08-12 21:46:44 +0300

    рефакторинг

---
+++ src/ingredients.java
@@ -0,0 +1,18 @@
+public class ingredients {
+    public static void main(String[] args) {
+
+        int eaters = 5; // сколько людей будут есть
+
+        int water = 3000; // миллилитров воды
+        float potatoes = 5; // картофелин
+        float chicken = 6; // куриных бёдер
+        float spices = 10; // ложек специй
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров(а) воды");
+        System.out.println((potatoes / eaters) + " картофелин(а/ы)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ки/ка) специй");
+
+    }
+}
